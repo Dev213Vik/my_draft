2b294fac5e57ec2485cafb3fd61011c8aba223b7
Dev213Vik <48687238+Dev213Vik@users.noreply.github.com>
2020-12-14 19:48:28 +0400

    AL 50%

--- portfolio/tree/js/script.js
+++ portfolio/tree/js/script.js
@@ -1,3 +1,5 @@
+include: ('swiper-bundle.min.js');
+
 function ibg(){
 	let ibg=document.querySelectorAll('.ibg');
 		for (var i = 0; i < ibg.length; i++) {
@@ -8,12 +10,24 @@ function ibg(){
 }
 ibg();
 
-
 document.querySelector('.icon-menu').onclick = function(event) {
 	document.querySelector('.icon-menu').classList.toggle('active');
 	document.querySelector('.menu__body').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
-
 }
+
+
+new Swiper('.slaider',{
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	},
+	slideToClickedSlide: true,
+});
 
 
