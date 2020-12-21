12fa2141a89e3cab16d78691c8c1d8e43c2dd2d3
Dev213Vik <48687238+Dev213Vik@users.noreply.github.com>
2020-12-21 21:05:15 +0400

    GRID 50%

--- portfolio/five(grid)/js/script.js
+++ portfolio/five(grid)/js/script.js
@@ -1,4 +1,8 @@
-
+document.querySelector('.header__burger').onclick = function(event) {
+	document.querySelector('.header__menu').classList.toggle('active');
+	document.querySelector('.header__burger').classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+}
 
 
 
